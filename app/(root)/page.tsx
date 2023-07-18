0ba2359f1dde75eb273ec3840332fd510f8fee56
sanjay.thakur <sanjay.thakur@dghub.io>
2023-07-18 16:49:10 +0545

    prisma

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  return <div></div>;
+};
+export default SetupPage;
